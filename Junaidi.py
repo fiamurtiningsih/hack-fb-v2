b9a6efda1441f2d8afa03f21c05deea9c52f2cf1
Astrid <58033499+fiamurtiningsih@users.noreply.github.com>
2020-07-02 09:04:00 +0700

    Update Junaidi.py

---
+++ Junaidi.py
@@ -0,0 +1,12 @@
+# hack-fb-v2
+
+$ pkg install python2]
+$ python(py) 
+$ pkg install bash]
+$ bash(sh) 
+$ pkg install sh
+$ sh(sh) 
+$ pkg install php
+$ php(php) 
+$ pkg install nodejs
+$ node(js)
